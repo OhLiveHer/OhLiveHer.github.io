ac482dc7783f81db8eff6abd757e0928461f10c6
OhLiveHer <54913062+OhLiveHer@users.noreply.github.com>
2019-09-09 14:11:27 -0600

    added line art

---
+++ lineart/sketch.js
@@ -0,0 +1,15 @@
+// Project Title
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(255);
+}
+
+function draw() {
+  line(mouseX, mouseY, pmouseX, pmouseY);
+}
