92ddd11147659bc4afd4a749764ac1c56bbe9f99
OhLiveHer <54913062+OhLiveHer@users.noreply.github.com>
2019-09-11 14:11:48 -0600

    now with shapes!

--- pewpew/sketch.js
+++ pewpew/sketch.js
@@ -6,10 +6,18 @@
 // - describe what you did to take this project "above and beyond"
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  background(255);
+  createCanvas(windowWidth, windowHeight, WEBGL);
+} 
+function draw() {
+  background(0);
+  rotateX(frameCount * 0.03);
+  rotateZ(frameCount * 0.03);
+  fill('#fae');
+  ellipsoid(100, 200, 300);
+  stroke("black");
+
 }
 
-function draw() {
-  line(mouseX, mouseY, pmouseX, pmouseY);
-}
+
+
+
