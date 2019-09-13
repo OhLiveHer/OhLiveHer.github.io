08453d32c132243133e59784de128df91f7a9ecb
OhLiveHer <54913062+OhLiveHer@users.noreply.github.com>
2019-09-13 13:08:18 -0600

    a

--- pewpew/sketch.js
+++ pewpew/sketch.js
@@ -4,20 +4,25 @@
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
-
+//<img src="west.png" id="image" onclick="rotate();"></img>
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
 } 
 function draw() {
   background(0);
-  rotateX(frameCount * 0.03);
-  rotateZ(frameCount * 0.03);
-  fill('#fae');
-  ellipsoid(100, 200, 300);
-  stroke("black");
+  rotateX(frameCount * 0.2);
+  rotateZ(frameCount * 0.2);
+  //cone(150, 210);
+  //ellipsoid(200, 300, 300);
+  //<img src="west.png" alt="west" width="60" height="35"></img>
+  //fill('#fae')
+  
 
 }
-
+<button id="button">Click me!</button>
+<div id="west">
+  <img src="west.png" id="image" onclick="rotate();"></img>
+</div> 
 
 
 
