00f61a785ffb204ffae5a09d68aed587367594fa
OhLiveHer <54913062+OhLiveHer@users.noreply.github.com>
2019-09-20 14:10:03 -0600

    now theres drake!

--- secondproject/sketch.js
+++ secondproject/sketch.js
@@ -4,21 +4,40 @@ let h = 50;
 let x;
 let y;
 let w;
+let dx = 5;
+let dy = 5;
 function preload() {
   west = loadImage("assets/west1.png");
   west2 = loadImage("assets/west2.png");
-  drake = loadImage("assets/drake.png")
+  drake = loadImage("assets/drake.png");
+  br = loadImage("assets/br.jpg");
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  x = width 
+  x = width / 4
   y = height
+  y1 = height / 2
+  r = 100
+  
   
 }
 
 function draw() {
-  background('#fae');
+  background('#FF69B4');
+  x += dx;
+  y1 += dy;
+
+  if (x > width / 2 - r/2 || x < 0 + r/2) {
+    dx *= -1
+    }
+  
+  if (y1 > height - r/2 || y1 < 0 + r/2) {
+    dy  /=  -1
+  }
+
+  image(drake, x, y1, 250, 250)
+  imageMode(CENTER)
   
   if (keyIsPressed) {
 
@@ -34,43 +53,31 @@ function draw() {
     
     if (key === 'd') {
       image(west2, 100, h);
-      stroke(50);
-      fill("red");
-      ellipse(x, y, 50);
-      x = x + 6
+
         
       
     }
     else {
-      image(west, 100, h)
+      image(west, 100, h);
     }
+}
+    
 
 
-
-      // if (mouseIsPressed) {
-      //   if (mouseButton === LEFT) {
-      //     stroke(50);
-      //     fill("red");
-      //     ellipse(x, y, 50, h)
-      //     x = x + 6;
-      // } 
-   
+  
+  
+  
 
             
-          // }
-        // }
+
+        
       
   
     
     
   
    
-   
-   
-   
-   
-   
-}
+
 
 
 
