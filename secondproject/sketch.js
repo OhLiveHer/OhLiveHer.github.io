a7f1bca98d012906b403c4036644b50105938c0b
OhLiveHer <54913062+OhLiveHer@users.noreply.github.com>
2019-09-25 14:12:01 -0600

    now it can shoot more then once

--- secondproject/sketch.js
+++ secondproject/sketch.js
@@ -7,6 +7,9 @@ let y;
 let w;
 let dx = 5;
 let dy = 5;
+let w2 = 200;
+cer = true;
+
 function preload() {
   west = loadImage("assets/west1.png");
   west2 = loadImage("assets/west2.png");
@@ -20,6 +23,7 @@ function setup() {
   y = height
   y1 = height / 2
   r = 100
+  x1 = width / 4
   
   
   
@@ -28,6 +32,7 @@ function setup() {
 function draw() {
   imageMode(CORNER);
   background(br);
+  //drake randomly moves
   x += dx;
   y1 += dy;
 
@@ -38,13 +43,22 @@ function draw() {
   if (y1 > height - r/2 || y1 < 0 + r/2) {
     dy  /=  -1
   }
+  //decreases size of shot(by 10)
+  if (mouseIsPressed){
+    if (mouseButton === LEFT){
+      w2 = w2 -= 10;
+
+    }
+  }
+
+  
 
   
   imageMode(CENTER)
   image(drake, x, y1, 250, 250)
   
   if (keyIsPressed) {
-
+//movement
   }
     if (key === 'w') {
       h -= 4;
@@ -53,10 +67,21 @@ function draw() {
     }
     if (key === 's') {
       h += 4;
+      
     }
     
     if (key === 'd') {
       image(west2, 100, h);
+      if (cer = true)
+        circle(x1, h + 50, w2)
+        fill('red')
+        x1 = x1 *= 1.03;
+        (cer = false)
+        if (x1 > windowWidth){
+          cer = true;
+        
+      }
+
 
         
       
