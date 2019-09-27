7173ca7c0c844b573e35cf9bd57b340a7e58359d
OhLiveHer <54913062+OhLiveHer@users.noreply.github.com>
2019-09-27 13:33:14 -0600

    nu

---
+++ suchmore/sketch.js
@@ -0,0 +1,17 @@
+let state = "menu";
+
+function preload(){
+  wp = loadImage("wp.jpg");
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  x = width/2;
+  y = height/2;
+}
+
+function draw() {
+  background("#fae");
+
+
+}
