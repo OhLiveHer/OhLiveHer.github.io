a9eba0cab74f40f3baf7f484c267ed5abdfcdeed
OhLiveHer <54913062+OhLiveHer@users.noreply.github.com>
2019-10-24 14:14:22 -0600

    powpow

--- powpow/sketch.js
+++ powpow/sketch.js
@@ -1,47 +1,70 @@
-let grid = [];
+let grid;
+let rows = 30;
+let cols = 30;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  grid = create2dArray(20, 20);
-}
-function draw() {
-  background(220)
-  displayGrid(grid);
+  grid = createRandom2dArray(cols, rows);
 }
 
-function displayGrid(theGrid) {
-  for (let y = 0; y < theGrid[0].length; y++) {
-    for (let x = 0; y < theGrid[0].length; y++){
-      if (theGrid[y][x] === 0) {
+function draw() {
+  background(220);
+  displayGrid(grid, rows, cols);
+}
+function keyIsPressed() {
+  if (key === 'r'){
+    grid = createRandom2dArray(cols, rows);
+  }
+  if (key === 'c'){
+    for (let x = 0; x < cols; x++) {
+      for(let y = 0; y < rows; y++) {
+        grid[y][x] = 1;
+      }
+    }
+  }
+}
+
+function mousePressed() {
+  let cellSize = width/cols;
+
+  let xCoord = floor(mouseX / cellSize);
+  let yCoord = floor(mouseY / cellSize);
+  
+  if (grid[yCoord][xCoord] === 1) {
+    grid[yCoord][xCoord] = 0;
+  }
+  else {
+    grid[yCoord][xCoord] = 1;
+  }
+}
+
+function displayGrid(grid, windowWidth, windowHeight) {
+  let cellSize = width / cols;
+  for (let y = 0; y < cols; y++) {
+    for (let x = 0; x < rows; x++) {
+      if (grid[y][x] === 0) {
+        fill(0);
+      }
+      else {
         fill(255);
       }
-      else {
-        fill(0)
-      }
-      let cellSize = width / theGrid[0].length;
-      rect(x * cellSize, y * cellSize, cellSize, cellSize);
+      rect(x*cellSize, y*cellSize, cellSize, cellSize);
     }
   }
 }
 
-function create2dArray(cols, rows) {
-  let someArray = [];
-  for (let i=0; i<cols; i++) {
-    someArray.push([]);
-    for (let j=0; j<rows; j++) {
+function createRandom2dArray(cols, rows) {
+  let randomGrid = [];
+  for (let x = 0; x < cols; x++) {
+    randomGrid.push([]);
+    for (let y = 0; y < rows; y++) {
       if (random(100) < 50) {
-        someArray[i].push(1);
+        randomGrid[x].push(1);
       }
       else {
-        someArray[i].push(1);
+        randomGrid[x].push(0);
       }
     }
   }
-  return someArray;
+  return randomGrid;
 }
-  
-
-
-
-
-
