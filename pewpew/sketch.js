f894f189243884cc2931b3a998efb9296a705d63
OhLiveHer <54913062+OhLiveHer@users.noreply.github.com>
2019-11-21 14:16:09 -0600

    uzi moved (he no longer does)  :(

--- pewpew/sketch.js
+++ pewpew/sketch.js
@@ -1,56 +1,46 @@
-// Project Title
-// Your Name
-// Date
-//
-// Extra for Experts:
-// Kanye West shoots out random objects from his mouth at drake... 
-//except theres a random object that moves in the center of the screen that can block them...
-let trav;
-let aaa;
-function preload(){
-  trav = loadImage("assets/trav.jpg")
+let uzi;
+let br;
+let x;
+let y;
+let h = 50;
+let w = 50;
+
+function preload() {
+  uzi = loadImage("assets/uzi.png");
+  br = loadImage("assets/pewpew.jpg");
 }
 
-function setup() {
+function setup(){
   createCanvas(windowWidth, windowHeight);
-  
+  x = width/2;
+  y = height/2;
+  //imageMode(Corner);
+  background(0);
 }
-
 
 function draw() {
-  background('#fae');
-  aaa = makeGrayscale(trav);
-  
-  image(aaa, 0, 0)
-}
-
-function makeGrayscale(sourceImage) {
-  let img = createImage(sourceImage.width, sourceImage.height);
-
-  img.loadPixels();
-  sourceImage.loadPixels();
-  
-  for (let x = 0; x < sourceImage.width; x++) {
-    for (let y = 0; y < sourceImage.height; y++) {
-      let p = sourceImage.get(x, y);
-      let r = red(p);
-      let g = green(p);
-      let b = blue(p);
-      
-      let average = (r + g + b) / 3;
-      
-      if  (dist(mouseX, mouseY, x, y) > 100){
-        img.set(x, y, color(average, average, average));
-      }
-      else img.set(x, y, color(r, g, b));
+  uzi();
+  if (keyIsPressed) {
+    if (keyCode === 'w') {
+      h -= 4;
     }
+    if (keyCode === 's') {
+      h += 4;
+    }
+    if (keyCode === 'd') {
+      w += 4;
+    }
+    if (keyCode === 'a') {
+      w -= 4;
   }
-  img.updatePixels();
-  return img;
+  
 }
 
-
-
+}
+function uzi() {
+  image(uzi, w, h, 50, 50);
+}
+    
 
 
 
