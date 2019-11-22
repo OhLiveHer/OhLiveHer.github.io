a66208e7842203b93557e3cfd59f614160592b95
OhLiveHer <54913062+OhLiveHer@users.noreply.github.com>
2019-11-22 14:13:09 -0600

    things work

--- pewpew/sketch.js
+++ pewpew/sketch.js
@@ -1,46 +1,80 @@
 let uzi;
 let br;
-let x;
-let y;
-let h = 50;
-let w = 50;
+let x = 200;
+let y = 200;
 
+
+
+let movingLeft = false;
+let movingRight = false;
+let isJump = false;
+let isFalling = true;
 function preload() {
   uzi = loadImage("assets/uzi.png");
   br = loadImage("assets/pewpew.jpg");
 }
 
 function setup(){
+  w = windowWidth/2;
+  h = windowHeight/2;
   createCanvas(windowWidth, windowHeight);
-  x = width/2;
-  y = height/2;
   //imageMode(Corner);
-  background(0);
-}
-
-function draw() {
-  uzi();
-  if (keyIsPressed) {
-    if (keyCode === 'w') {
-      h -= 4;
-    }
-    if (keyCode === 's') {
-      h += 4;
-    }
-    if (keyCode === 'd') {
-      w += 4;
-    }
-    if (keyCode === 'a') {
-      w -= 4;
-  }
   
 }
 
+
+function draw() {
+  imageMode(CORNER);
+  background(br);
+  moveUzi();
+  fill(0);
+  imageMode(CENTER)
+  image(uzi, x, y, 350, 350);
 }
-function uzi() {
-  image(uzi, w, h, 50, 50);
+
+function moveUzi() {
+  if (movingLeft) {
+    x -=3;
+  }
+  if (movingRight) {
+    x +=3;
+  }
+  if (isJump) {
+    y -= 10;
+  }
+  if (isFalling) {
+    y+= 5;
+  }
 }
-    
+
+function keyPressed() {
+  if (key === "a") {
+    movingLeft = true;
+  }
+  if (key === "d") {
+    movingRight = true;
+  }
+  if (key ==="w") {
+    isJump = true;
+    isFalling = false;
+  }
+}
+
+function keyReleased() {
+  if (key === "a") {
+    movingLeft = false;
+  }
+  if (key === "d") {
+    movingRight = false;
+  }
+  if (key === "w") {
+    isJump = false;
+    isFalling = true;
+  }
+}
+
+//function uziAttack();
+
 
 
 
